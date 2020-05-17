a7adf456bbc5ad18aae442eb5ce5d2332d91e031
Vasli Saidmuradov <vasli.said@gmail.com>
2020-05-18 01:17:27 +0600

    feat: 08 task

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -126,8 +126,13 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return (
+    (rect1.top < rect2.top + rect2.height)
+    && (rect1.left < rect2.left + rect2.width)
+    && (rect2.top < rect1.top + rect1.height)
+    && (rect2.left < rect1.left + rect1.width)
+  );
 }
 
 
@@ -157,8 +162,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  return Math.sqrt((circle.center.x - point.x) ** 2
+    + (circle.center.y - point.y) ** 2) < circle.radius;
 }
 
 
@@ -204,8 +210,14 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  const aa = (isStartIncluded) ? '[' : '(';
+  const bb = (isEndIncluded) ? ']' : ')';
+
+  return `${aa + min}, ${max + bb}`;
 }
 
 
@@ -263,8 +275,22 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const str = `${ccn}`;
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    let cardNum = +str[i];
+
+    if (!((str.length - i) % 2)) {
+      cardNum *= 2;
+      if (cardNum > 9) {
+        cardNum -= 9;
+      }
+    }
+    sum += cardNum;
+  }
+  return sum % 10 === 0;
 }
 
 /**
